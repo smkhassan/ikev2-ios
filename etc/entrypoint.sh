5e15d22bb70f518412ca21852468a2335345ed11
smkhassan <futureco@email.cz>
2020-06-21 12:25:44 +0200

    fix3

---
+++ etc/entrypoint.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+
+git clone https://github.com/letsencrypt/letsencrypt ./letsencrypt
+
+
+
+./letsencrypt/letsencrypt-auto certonly --standalone --email sami.hassan@vpnmaze.com --agree-tos --no-eff-email -d test.vpnmaze.online
+
+ln -f -s "/etc/letsencrypt/live/test.vpnmaze.online/cert.pem"    /etc/ipsec.d/certs/cert.pem
+ln -f -s "/etc/letsencrypt/live/test.vpnmaze.online/privkey.pem" /etc/ipsec.d/private/privkey.pem
+ln -f -s "/etc/letsencrypt/live/test.vpnmaze.online/chain.pem"   /etc/ipsec.d/cacerts/chain.pem
